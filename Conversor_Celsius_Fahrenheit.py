ec28eaf5a558b126e6aba1f658c3056005e29b41
Guilherme Presot <89171084+guilhermepesot@users.noreply.github.com>
2026-03-02 16:01:51 -0300

    Add Celsius to Fahrenheit converter script

---
+++ Conversor_Celsius_Fahrenheit.py
@@ -0,0 +1,4 @@
+celsius = int(input("Escreva a temperatura em Celsius: "))
+fahrenheit = ( celsius * 9/5 ) + 32  #formula de celsius para fahrenheit
+
+print("A temperatura em celsius: {}, em fahrenheit é: {}".format( celsius , fahrenheit))
